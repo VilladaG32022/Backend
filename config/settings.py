035f23234e63b43947fdc1399d6e88fa942ea843
hernan <hernanchazarreta9@gmail.com>
2022-09-20 12:49:56 -0300

    admin site

--- config/settings.py
+++ config/settings.py
@@ -73,6 +73,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Library Admin",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Cocinamos por los niños",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Library",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": "static/logo_color.png",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": "static/logo_color.png",
+
+    # Welcome text on the login screen
+    "welcome_sign": "Bienvenido a la administracion de 'Cocinamos por los niños'",
+
+    # Copyright on the footer
+    "copyright": "Acme Library Ltd",
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
