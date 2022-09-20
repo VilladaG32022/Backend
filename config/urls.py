6481b2d240ffc785bac763b894ad4d0b97f9e821
hernan <hernanchazarreta9@gmail.com>
2022-09-20 12:22:59 -0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.contrib import admin
 from api import *
-admin.site.site_header = "Cocinamos por los niños"
+admin.site.site_header = "CocinamosPorLosNiños"
 
 from django.conf import settings
 from django.conf.urls.static import static
