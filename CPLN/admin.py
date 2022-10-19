aee7ba967bd9c6c7f38c64d8a771a29b7a9cd0f6
hernan <hernanchazarreta9@gmail.com>
2022-10-19 08:40:24 -0300

    WF - new actions

--- CPLN/admin.py
+++ CPLN/admin.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import *
 from django.apps import apps
+from api.serializers import CandidateSerializer
+from rest_framework.response import Response
+from rest_framework import status
     
 my_models = apps.get_models()
 
@@ -17,7 +20,17 @@ class CandidateAdmin(admin.ModelAdmin):
 
     actions = ['add_volunteer']
 
-    def add_volunteer(self):
+    def add_volunteer(modeladmin, request, queryset):
+        for candidate in queryset:
+            serializeobj = CandidateSerializer(data=request.candidate, many=True)
+            if serializeobj.is_valid():
+                serializeobj.save()
+                candidate.delete()
+                content = {'Candidate created OK': 'Candidate created'}
+                return Response(content, status=status.HTTP_200_OK)
+            else:
+                content = {'BAD_REQUEST': 'Invalid Candidate'}
+                return Response(content, status=status.HTTP_400_BAD_REQUEST)
         pass
 
 @admin.register(New)
