239d5e1aec1996b34fc78f6705c645d6f0562925
hernan <hernanchazarreta9@gmail.com>
2022-10-25 10:46:56 -0300

    hiding logEntries

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ JAZZMIN_SETTINGS = {
     # Copyright on the footer
     "copyright": "ITS Villada",
     
-    "hide_apps": ["contenttypes","sessions","adminlogs"],
+    "hide_apps": ["contenttypes","sessions","adminlog"],
 
     "hide_models": ["CPLN.Neighborhood","auth.Permission", "auth.Group"],
 
