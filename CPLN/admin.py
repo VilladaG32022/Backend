d5cad505b8533320f77bad8ffac3024ddcf6f275
hernan <hernanchazarreta9@gmail.com>
2022-10-27 11:53:46 -0300

    WF - showing models

--- CPLN/admin.py
+++ CPLN/admin.py
@@ -10,20 +10,13 @@ my_models = apps.get_models()
 
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
-    list_display = ("last_name", "first_name", "neighborhood")
+    list_display = ("last_name", "first_name", "neighborhood", "status")
     ordering = ("last_name",)
     list_filter = ("neighborhood",)
     list_display_links = ("last_name",)
-    #list_editable = ("status",)
+    list_editable = ("status",)
     list_per_page = 10
-    actions = ["add_volunteer"]
 
-    def add_volunteer(modeladmin, request, queryset):
-        for candidate in queryset:
-            serializeobj = VolunteerSerializer(data=candidate)
-            if serializeobj.is_valid():
-                VolunteerSerializer(candidate).save()
-                candidate.delete()
 
 #modeladmin
 
