6b011b1c199c6916b64d79e68759f79afe1311cd
hernan <hernanchazarreta9@gmail.com>
2022-11-01 08:05:40 -0300

    WP - adding a button

--- CPLN/admin.py
+++ CPLN/admin.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import *
 from django.apps import apps
-from api.serializers import VolunteerSerializer
+from django.utils.html import format_html
     
 my_models = apps.get_models()
 
-
+#modeladmin
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "neighborhood", "status")
@@ -18,29 +18,26 @@ class CandidateAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
-#modeladmin
-
-
 @admin.register(New)
 class NewAdmin(admin.ModelAdmin):
     list_display = ("title", "description")
     list_filter = ("title",)
     list_per_page = 5
 
-# Register your models here.
-
 class IngredientInline(admin.StackedInline):
     extra = 1
     model = Ingredient
-from django.utils.html import format_html
+
 @admin.register(Lunch)
 class LunchAdmin(admin.ModelAdmin):
     def action(self, obj):
         if not obj:
             return
-        return format_html(f'<button type="button" onclick="/calc-lunch/{obj.id}">Calcular menu</button>')
+        return format_html(f'<button type="button" onclick="calc-lunch/{obj.id}">Calcular menu</button>')
     inlines =  (IngredientInline, )
     list_display = ('description', 'action')
+
+# Register your models here.
 
 
 
